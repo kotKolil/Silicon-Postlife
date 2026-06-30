2ebdb017610c768ba0e2d8fb4fafda6ac25ea806
Михаил <ldxi1991@gmail.com>
2026-06-30 11:39:09 +0300

    working on better menu

--- src/scenes/menu.c
+++ src/scenes/menu.c
@@ -2,19 +2,58 @@
 #include <stddef.h>
 #include <raylib.h>
 #include <stdint.h>
+#include <stdio.h>
 
-const int screenWidth = 1920;
-const int screenHeight = 1080;
 
-static Font customFont;
-static bool FontLoaded = false;
-
+int screenTextDelta(char text[], int font_size, bool dim);
 
 void  MRender (int *levelN, Camera3D *cam) {
     ClearBackground(BLACK);
-	DrawText("press space to connect", screenWidth / 2 - MeasureText("press space to connect", GetScreenHeight()/35) / 2, 3 * screenHeight / 4, 40, WHITE);
-	SetTextureFilter(customFont.texture, TEXTURE_FILTER_BILINEAR);
-	DrawTextEx(customFont, "Silicon Postlife", (Vector2){GetScreenWidth()/16, GetScreenHeight()/2}, 100, 2.0f, GREEN);
+	DrawText(
+		"Silicon Postlife",
+			screenTextDelta("Silicon Postlife", 100, true) / 2,
+				screenTextDelta("Silicon Postlife", 100, false) / 2,
+						100,
+							GREEN
+								);
+
+	int startX = GetScreenWidth() / 6;
+	int startY = (screenTextDelta("start", 50, false) / 2) + startY;
+
+	int optionsX = 100;
+	int optionsY = startY + 120; 
+	int quitX = 100;
+
+	int quitY = optionsY + 120;
+	DrawText("start", startX, startY, 50, GREEN);
+	DrawRectangleLines(
+	    startX - 30, 
+	    startY - 15, 
+	    MeasureText("start", 50) + 60, 
+	    90, 
+	    RED
+	);
+
+
+	DrawText("options", startX, startY + 200, 50, GREEN);
+	DrawRectangleLines(
+	    startX - 30, 
+	    startY - 185, 
+	    MeasureText("options", 50) + 60, 
+	    90, 
+	    RED
+	);
+
+	DrawText("quit", startX, startY + 300, 50, GREEN);
+	DrawRectangleLines(
+	    startX - 30, 
+	    startY - 285, 
+	    MeasureText("quit", 50) + 60, 
+	    90, 
+	    RED
+	);
+
+	
 	if ( IsKeyPressed(KEY_SPACE) )  {
 		(*levelN)++; 
 	}
@@ -22,5 +61,23 @@ void  MRender (int *levelN, Camera3D *cam) {
 
 __attribute__((visibility("default"))) Scene menu =  { 
 	.n = 0,
-	.render = MRender,
+	.render = MRender,	
 };
+
+int screenTextDelta(char text[], int font_size, bool dim) {
+	// if dim true -> width, tho  height
+	// 1 function better 2
+	int delta;
+	if (dim) {
+		delta = GetScreenWidth() - MeasureText( text, font_size);
+		return delta;
+	}
+	else {
+		delta = GetScreenHeight() - font_size;
+		return delta;
+	}
+}
+
+	
+
+
